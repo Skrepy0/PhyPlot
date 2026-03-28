c3f9a9291671f19987600abc344163943a824429
Skrepy <703189721@qq.com>
2026-03-28 16:42:12 +0800

    Add frontend folder and updates

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+interface Data {
+  data:number
+}
+let buttonData=ref<number>(0)
+const inputData = 114514
+const getData = async () => {
+  const response = await $fetch<Data>('/api/process', {
+    method: 'POST',
+    body: { data: inputData }
+  })
+  if (response.data) {
+    buttonData.value = response.data
+    console.log(response.data)
+  }
+}
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <button @click="getData">
+    {{buttonData}}
+  </button>
   </div>
 </template>
+
